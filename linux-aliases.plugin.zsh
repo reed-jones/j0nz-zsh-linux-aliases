ce376a41d34662f026eedb8abd074a6fed3d5e81
Reed Jones <reedjones@reedjones.com>
2018-10-14 16:39:17 -0600

    Update linux-aliases.plugin.zsh

--- linux-aliases.plugin.zsh
+++ linux-aliases.plugin.zsh
@@ -1,2 +1,4 @@
+# removes ubuntu-link in case of wsl
+export PATH=`echo $PATH | tr ':' '\n' | grep -v ubuntu-link | tr '\n' ':'`
 
 alias xcopy="xclip -i -selection primary -filter | xclip -i -selection clipboard"
