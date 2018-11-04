4f3f1764a50767ae6c016a870542b68598df71bf
Reed Jones <reedjones@reedjones.com>
2018-11-04 09:07:12 -0700

    Update linux-aliases.plugin.zsh

--- linux-aliases.plugin.zsh
+++ linux-aliases.plugin.zsh
@@ -1,4 +1,5 @@
 # removes ubuntu-link in case of wsl
 export PATH=`echo $PATH | tr ':' '\n' | grep -v ubuntu-link | tr '\n' ':'`
+export PATH=$PATH:$HOME/.yarn/bin
 
 alias xcopy="xclip -i -selection primary -filter | xclip -i -selection clipboard"
